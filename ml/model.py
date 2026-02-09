a3715ab1c0beeb2a5c68e3576002f55c0372df5b
Somefatpickles <dustinjwhitehouse@gmail.com>
2026-02-09 13:25:35 -0800

    Added code for functions in alignment with Step 2

--- ml/model.py
+++ ml/model.py
@@ -1,12 +1,11 @@
 import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
-# TODO: add necessary import
+from sklearn.ensemble import RandomForestClassifier
 
-# Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
     """
-    Trains a machine learning model and returns it.
+    Trains a machine learning model (in this case, RandomForestClassifier) and returns it.
 
     Inputs
     ------
@@ -14,14 +13,17 @@ def train_model(X_train, y_train):
         Training data.
     y_train : np.array
         Labels.
+
     Returns
     -------
     model
         Trained machine learning model.
     """
-    # TODO: implement the function
-    pass
+    
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
 
+    return model
 
 def compute_model_metrics(y, preds):
     """
@@ -33,20 +35,22 @@ def compute_model_metrics(y, preds):
         Known labels, binarized.
     preds : np.array
         Predicted labels, binarized.
+
     Returns
     -------
     precision : float
     recall : float
     fbeta : float
     """
+
     fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
     recall = recall_score(y, preds, zero_division=1)
     return precision, recall, fbeta
 
-
 def inference(model, X):
-    """ Run model inferences and return the predictions.
+    """ 
+    Run model inferences and return the predictions.
 
     Inputs
     ------
@@ -54,16 +58,20 @@ def inference(model, X):
         Trained machine learning model.
     X : np.array
         Data used for prediction.
+
     Returns
     -------
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+
+    preds = model.predict(X)
+
+    return preds
 
 def save_model(model, path):
-    """ Serializes model to a file.
+    """ 
+    Serializes model to a file.
 
     Inputs
     ------
@@ -72,19 +80,23 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+
+    with open(path, "wb") as f:
+        pickle.dump(model, f)
 
 def load_model(path):
-    """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+    """ 
+    Loads pickle file from `path` and returns it.
+    """
 
+    with open(path, "rb") as f:
+        return pickle.load(f)
 
 def performance_on_categorical_slice(
     data, column_name, slice_value, categorical_features, label, encoder, lb, model
 ):
-    """ Computes the model metrics on a slice of the data specified by a column name and
+    """ 
+    Computes the model metrics on a slice of the data specified by a column name and
 
     Processes the data using one hot encoding for the categorical features and a
     label binarizer for the labels. This can be used in either training or
@@ -117,12 +129,18 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
+        data,
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+
+    preds = inference(model, X_slice)
+
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
+
     return precision, recall, fbeta
